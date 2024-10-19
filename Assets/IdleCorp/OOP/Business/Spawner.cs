6821995fea376bcb264a664ceec9b6d492eb5203
Andrea <andreaprezp@gmail.com>
2024-10-19 18:48:41 +0200

    Prepare Spawner script to spawn different elements in the world

---
+++ Assets/IdleCorp/OOP/Business/Spawner.cs
@@ -0,0 +1,45 @@
+using IdleCorp.OOP.Services;
+using IdleCorp.OOP.Services.EventsService;
+using UnityEngine;
+
+namespace IdleCorp.OOP.Business
+{
+    public class Spawner : MonoBehaviour
+    {
+        private void Start()
+        {
+            var eventsService = ServiceLocator.GetService<EventsService>();
+            //TODO: listen to events to spawn elements:
+            // eventsService.GetEvent<RobotProducedEvent>().AddListener(SpawnRobot);
+            // eventsService.GetEvent<HangarPurchasedEvent>().AddListener(SpawnHangar);
+            // eventsService.GetEvent<VehicleDeployedEvent>().AddListener(SpawnVehicle);
+            // eventsService.GetEvent<ShootingStarGeneratedEvent>().AddListener(SpawnShootingStar);
+        }
+
+        public void SpawnWorld()
+        {
+            //TODO: spawn Mine
+            //TODO: spawn Hangars
+        }
+
+        private void SpawnRobot()
+        {
+            //TODO
+        }
+
+        private void SpawnHangar(int hangarId, int positionId)
+        {
+            //TODO
+        }
+
+        private void SpawnVehicle()
+        {
+            //TODO
+        }
+
+        private void SpawnShootingStar()
+        {
+            //TODO
+        }
+    }
+}
