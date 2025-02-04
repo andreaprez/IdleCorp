02953c7e10704da90827c6a3ab1992348576c0dc
Andrea <andreaprezp@gmail.com>
2025-02-04 01:28:32 +0100

    Modify MovementSystem to destroy entity once it reaches its target

--- Assets/IdleCorp/ECS/Systems/MovementSystem.cs
+++ Assets/IdleCorp/ECS/Systems/MovementSystem.cs
@@ -1,6 +1,8 @@
 using IdleCorp.ECS.Components;
 using ProjectDawn.Navigation;
 using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
 
 namespace IdleCorp.ECS.Systems
 {
@@ -8,19 +10,34 @@ namespace IdleCorp.ECS.Systems
     {
         public void OnCreate(ref SystemState state)
         {
+            state.RequireForUpdate<BeginFixedStepSimulationEntityCommandBufferSystem.Singleton>();
         }
 
         public void OnUpdate(ref SystemState state)
         {
-            foreach (var (target, agent) 
-                     in SystemAPI.Query<RefRO<MovementComponent>, RefRW<AgentBody>>())
+            foreach (var (transform, movement, agent, entity) 
+                     in SystemAPI.Query<RefRO<LocalToWorld>, RefRO<MovementComponent>, RefRW<AgentBody>>().WithEntityAccess())
             {
-                agent.ValueRW.SetDestination(target.ValueRO.TargetPosition);
+                agent.ValueRW.SetDestination(movement.ValueRO.TargetPosition);
+                if (HasReachedTarget(transform, movement, agent))
+                {
+                    var ecb = SystemAPI.GetSingleton<BeginFixedStepSimulationEntityCommandBufferSystem.Singleton>()
+                        .CreateCommandBuffer(state.WorldUnmanaged);
+                    ecb.DestroyEntity(entity);
+                }
             }
         }
 
         public void OnDestroy(ref SystemState state)
         {
+        }
+
+        private bool HasReachedTarget(RefRO<LocalToWorld> transform, RefRO<MovementComponent> movement,
+            RefRW<AgentBody> agent)
+        {
+            var distance = agent.ValueRO.Destination - transform.ValueRO.Position;
+            return distance.x < movement.ValueRO.TargetReachedThreshold
+                   && distance.y < movement.ValueRO.TargetReachedThreshold;
         }
     }
 }
