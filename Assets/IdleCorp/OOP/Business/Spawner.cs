91fcf3cc44b9f8617e385ea4f1467749ba2f7b48
Andrea <andreaprezp@gmail.com>
2025-02-04 01:29:30 +0100

    Implement SpawnRobots() in Spawner

--- Assets/IdleCorp/OOP/Business/Spawner.cs
+++ Assets/IdleCorp/OOP/Business/Spawner.cs
@@ -1,13 +1,23 @@
+using IdleCorp.ECS.Components;
 using IdleCorp.OOP.Services;
 using IdleCorp.OOP.Services.Events;
+using Unity.Entities;
 using UnityEngine;
 
 namespace IdleCorp.OOP.Business
 {
     public class Spawner : MonoBehaviour
     {
+        private EntityManager _entityManager;
+        private Entity _robotSpawnEntity;
+
         private void Start()
         {
+            _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            var robotSpawnQuery = World.DefaultGameObjectInjectionWorld.EntityManager
+                .CreateEntityQuery(ComponentType.ReadOnly<RobotSpawnComponent>());
+            _robotSpawnEntity = robotSpawnQuery.GetSingletonEntity();
+
             var eventsService = ServiceLocator.GetService<EventsService>();
             //TODO: listen to events to spawn elements:
             // eventsService.GetEvent<RobotProducedEvent>().AddListener(SpawnRobot);
@@ -22,9 +32,16 @@ namespace IdleCorp.OOP.Business
             //TODO: spawn Hangars
         }
 
-        private void SpawnRobot()
+        private void SpawnRobots(int amount)
         {
-            //TODO
+            var spawnComponent = _entityManager.GetComponentData<RobotSpawnComponent>(_robotSpawnEntity);
+            var newComponent = new RobotSpawnComponent()
+            {
+                RobotPrefab = spawnComponent.RobotPrefab,
+                SpawnPosition = spawnComponent.SpawnPosition,
+                AmountToSpawn = spawnComponent.AmountToSpawn + amount
+            };
+            _entityManager.SetComponentData(_robotSpawnEntity, newComponent);
         }
 
         private void SpawnHangar(int hangarId, int positionId)
