09e2bed03e52d55b074868f86ca20a223200c7ac
Andrea <andreaprezp@gmail.com>
2025-03-04 23:04:41 +0100

    Check and substract Funds to produce robots

--- Assets/IdleCorp/OOP/Business/Factory/FactoryButton.cs
+++ Assets/IdleCorp/OOP/Business/Factory/FactoryButton.cs
@@ -1,3 +1,4 @@
+using IdleCorp.OOP.Persistence.Currencies;
 using IdleCorp.OOP.Persistence.Factory;
 using IdleCorp.OOP.Services;
 using IdleCorp.OOP.Services.Events;
@@ -18,12 +19,14 @@ namespace IdleCorp.OOP.Business.Factory
 
         private EventsService _eventsService;
         private FactoryData _factoryData;
+        private CurrenciesData _currenciesData;
         private float _cooldownTimer;
 
         private void Start()
         {
             _eventsService = ServiceLocator.GetService<EventsService>();
             _factoryData = ServiceLocator.GetService<UserDataService>().GetData<FactoryData>();
+            _currenciesData = ServiceLocator.GetService<UserDataService>().GetData<CurrenciesData>();
         }
 
         private void Update()
@@ -34,15 +37,23 @@ namespace IdleCorp.OOP.Business.Factory
                 _factoryData.SetProductionCurrentCapacity(_factoryData.ProductionCurrentCapacity + 1);
             }
             cooldownFill.fillAmount = (float)_factoryData.ProductionCurrentCapacity / _factoryData.ProductionMaxCapacity;
-            button.interactable = _factoryData.ProductionCurrentCapacity >= _factoryData.ProductionQuantity;
+            button.interactable = CanProduce();
             _cooldownTimer += Time.deltaTime;
         }
 
         public void OnPressed()
         {
-            // TODO: check coins
+            if (!CanProduce())
+                return;
             _eventsService.GetEvent<RobotProducedEvent>().Trigger(_factoryData.ProductionQuantity);
             _factoryData.SetProductionCurrentCapacity(_factoryData.ProductionCurrentCapacity - _factoryData.ProductionQuantity);
+            _currenciesData.ModifyFunds(-_factoryData.ProductionCost);
+        }
+
+        private bool CanProduce()
+        {
+            return _factoryData.ProductionCurrentCapacity >= _factoryData.ProductionQuantity
+                   && _currenciesData.Funds >= _factoryData.ProductionCost;
         }
     }
 }
