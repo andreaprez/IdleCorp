61bccfd918121c27448324ee461b0c0b9e119a7a
Andrea <andreaprezp@gmail.com>
2026-02-27 19:56:48 +0100

    Create generic animations for popups

---
+++ Assets/IdleCorp/OOP/Business/UI/Animations/AnimationTriggerKeys.cs
@@ -0,0 +1,8 @@
+namespace IdleCorp.OOP.Business.UI.Animations
+{
+    public static class AnimationTriggerKeys
+    {
+        public const string POPUP_OPEN = "Open";
+        public const string POPUP_CLOSE = "Close";
+    }
+}
